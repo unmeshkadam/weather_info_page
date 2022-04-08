ebb5dc8bce548cba246937f90ae75a7a59bb4bba
unmeshkadam <89571628+unmeshkadam@users.noreply.github.com>
2022-04-08 14:47:55 +0530

    Create weather_info.js

---
+++ weather_info.js
@@ -0,0 +1,33 @@
+
+	function showTime(){
+    var date = new Date();
+	const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+let day = weekday[date.getDay()];
+
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    
+    var time = h + ":" + m + " " + session;
+    document.getElementById("clockDisplay").innerText = day+ ", "+time;
+  
+    
+    setTimeout(showTime, 1000);
+    
+}
+showTime();
+	
